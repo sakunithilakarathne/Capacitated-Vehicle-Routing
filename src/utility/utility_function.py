5c27754840c552db2e4f0bd0d1c0a32e949dd844
Sakuni Thilakarathne <scsthilakarathne@gmail.com>
2025-10-17 09:45:17 +0530

    Add changes to load function

--- src/utility/utility_function.py
+++ src/utility/utility_function.py
@@ -9,9 +9,9 @@ from tqdm import tqdm
 
 def load_preprocessed_data(artifact_dir):
     """Load preprocessed CVRP data from artifact directory"""
-    dist_matrix = np.load(os.path.join(artifact_dir, 'distance_matrix.npy'))
-    demands = np.load(os.path.join(artifact_dir, 'demands.npy'))
-    coords = np.load(os.path.join(artifact_dir, 'coords.npy'))
+    dist_matrix = np.load(f"{artifact_dir}/distance_matrix.npy")
+    demands = np.load(f"{artifact_dir}/demands.npy")
+    coords = np.load(f"{artifact_dir}/coords.npy")
     return dist_matrix, demands, coords
 
 
